9f8b13f1c983276d2ea6f795f4e6b30d218a7f32
Clemens Park <clemens.park@gmail.com>
2015-09-01 02:14:16 -0400

    extracts a bunch of components

--- js/app.js
+++ js/app.js
@@ -10,43 +10,11 @@ export default class App extends Component {
         </header>
 
         <section className="main">
-          <input className="toggle-all" type="checkbox"></input>
-          <label htmlFor="toggle-all">Mark all as complete</label>
-          <ul className="todo-list">
-            <li className="completed">
-              <div className="view">
-                <input className="toggle" type="checkbox" checked></input>
-                <label>Taste JavaScript</label>
-                <button className="destroy"></button>
-              </div>
-              <input className="edit" value="Create a TodoMVC template"></input>
-            </li>
-            <li>
-              <div className="view">
-                <input className="toggle" type="checkbox"></input>
-                <label>Buy a unicorn</label>
-                <button className="destroy"></button>
-              </div>
-              <input className="edit" value="Rule the web"></input>
-            </li>
-          </ul>
+          <ToggleAll></ToggleAll>
+          <TodoList></TodoList>
         </section>
 
-        <footer className="footer">
-          <span className="todo-count"><strong>0</strong> item left</span>
-          <ul className="filters">
-            <li>
-              <a className="selected" href="#/">All</a>
-            </li>
-            <li>
-              <a href="#/active">Active</a>
-            </li>
-            <li>
-              <a href="#/completed">Completed</a>
-            </li>
-          </ul>
-          <button className="clear-completed">Clear completed</button>
-        </footer>
+        <TodoFooter></TodoFooter>
       </section>
     );
   }
@@ -56,6 +24,64 @@ class AddTodo extends Component {
   render() {
     return (
       <input className="new-todo" placeholder="What needs to be done?" autofocus></input>
+    );
+  }
+}
+
+class ToggleAll extends Component {
+  render() {
+    return (
+      <div>
+        <input className="toggle-all" type="checkbox"></input>
+        <label htmlFor="toggle-all">Mark all as complete</label>
+      </div>
+    );
+  }
+}
+
+class TodoList extends Component {
+  render() {
+    return (
+      <ul className="todo-list">
+        <li className="completed">
+          <div className="view">
+            <input className="toggle" type="checkbox" checked></input>
+            <label>Taste JavaScript</label>
+            <button className="destroy"></button>
+          </div>
+          <input className="edit" value="Create a TodoMVC template"></input>
+        </li>
+        <li>
+          <div className="view">
+            <input className="toggle" type="checkbox"></input>
+            <label>Buy a unicorn</label>
+            <button className="destroy"></button>
+          </div>
+          <input className="edit" value="Rule the web"></input>
+        </li>
+      </ul>
+    );
+  }
+}
+
+class TodoFooter extends Component {
+  render() {
+    return (
+      <footer className="footer">
+        <span className="todo-count"><strong>0</strong> item left</span>
+        <ul className="filters">
+          <li>
+            <a className="selected" href="#/">All</a>
+          </li>
+          <li>
+            <a href="#/active">Active</a>
+          </li>
+          <li>
+            <a href="#/completed">Completed</a>
+          </li>
+        </ul>
+        <button className="clear-completed">Clear completed</button>
+      </footer>
     );
   }
 }
