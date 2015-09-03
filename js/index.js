f62382aef780b803de4e5ae6a892fe0740498f1b
Clemens Park <clemens.park@gmail.com>
2015-09-03 02:23:40 -0400

    sets up redux dev tools!

--- js/index.js
+++ js/index.js
@@ -3,9 +3,18 @@ import { Provider } from 'react-redux';
 import App from './containers/App';
 import { store } from './store/TodoStore';
 
+// React components for Redux DevTools
+import { DevTools, DebugPanel, LogMonitor } from 'redux-devtools/lib/react';
+
 React.render(
-  <Provider store={store}>
-    {() => <App />}
-  </Provider>,
+  <div>
+    <Provider store={store}>
+      {() => <App />}
+    </Provider>
+    <DebugPanel top right bottom>
+      <DevTools store={store} monitor={LogMonitor} />
+    </DebugPanel>
+  </div>
+  ,
   document.getElementById('root')
 );
--- js/store/TodoStore.js
+++ js/store/TodoStore.js
@@ -1,4 +1,15 @@
-import { createStore } from 'redux';
+// Redux utility functions
+import { compose, createStore, applyMiddleware } from 'redux';
 import reducer from '../reducers';
 
-export const store = createStore(reducer);
+// Redux DevTools store enhancers
+import { devTools, persistState } from 'redux-devtools';
+
+const finalCreateStore = compose(
+  // Provides support for DevTools:
+  devTools(),
+  // Lets you write ?debug_session=<name> in address bar to persist debug sessions
+  persistState(window.location.href.match(/[?&]debug_session=([^&]+)\b/))
+)(createStore);
+
+export const store = finalCreateStore(reducer);
