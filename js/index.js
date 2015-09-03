1462ea4cb94184a1ac3dbe8352e93496d51d517f
Clemens Park <clemens.park@gmail.com>
2015-09-03 01:49:18 -0400

    removes unused code

--- js/index.js
+++ js/index.js
@@ -3,14 +3,6 @@ import { Provider } from 'react-redux';
 import App from './containers/App';
 import { store } from './store/TodoStore';
 
-var todoItems = [
-  { description: "todo1", completed: true },
-  { description: "todo2", completed: false },
-  { description: "todo3", completed: false }
-];
-
-/* React.render(<TodoApp todoItems={todoItems} />, document.getElementById('root')); */
-
 React.render(
   <Provider store={store}>
     {() => <App />}
