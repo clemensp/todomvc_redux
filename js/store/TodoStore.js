0fa6b7ee0dd4296990a04e2da9120241e7abd1e8
Clemens Park <clemens.park@gmail.com>
2015-09-07 01:57:37 -0400

    does not persist the edit state

--- js/store/TodoStore.js
+++ js/store/TodoStore.js
@@ -1,13 +1,26 @@
 // Redux utility functions
 import { compose, createStore, applyMiddleware } from 'redux';
 import persistToLocalStorage from 'redux-localstorage';
+import _ from 'lodash';
 import reducer from '../reducers';
 
 // Redux DevTools store enhancers
 import { devTools, persistState } from 'redux-devtools';
 
 const finalCreateStore = compose(
-  persistToLocalStorage(undefined, {key: "todomvc-redux"}),
+  persistToLocalStorage(
+    undefined, {
+    key: "todomvc-redux",
+    slicer: function(paths) {
+      return function(state) {
+        let subset = _.extend({}, state);
+        subset.todoItems = _.map(subset.todoItems, function(i) {
+          return _.omit(i, "mode");
+        });
+        return subset;
+      }
+    }
+  }),
   // Provides support for DevTools:
   devTools(),
   // Lets you write ?debug_session=<name> in address bar to persist debug sessions
