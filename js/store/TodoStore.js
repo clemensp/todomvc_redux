5adeb59c2d3339f5372e099a2d0efb284bdc1a75
Clemens Park <clemens.park@gmail.com>
2015-09-07 01:46:37 -0400

    persists to local storage

--- js/store/TodoStore.js
+++ js/store/TodoStore.js
@@ -1,11 +1,13 @@
 // Redux utility functions
 import { compose, createStore, applyMiddleware } from 'redux';
+import persistToLocalStorage from 'redux-localstorage';
 import reducer from '../reducers';
 
 // Redux DevTools store enhancers
 import { devTools, persistState } from 'redux-devtools';
 
 const finalCreateStore = compose(
+  persistToLocalStorage(undefined, {key: "todomvc-redux"}),
   // Provides support for DevTools:
   devTools(),
   // Lets you write ?debug_session=<name> in address bar to persist debug sessions
